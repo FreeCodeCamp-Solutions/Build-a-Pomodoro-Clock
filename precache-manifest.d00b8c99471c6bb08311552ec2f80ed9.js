302a9ddcd2491b49f6a7abea4afd82d5007643fb
AFiF_AhsaN <47252914+AfifOne@users.noreply.github.com>
2020-12-09 14:49:34 +0600

    Updates

---
+++ precache-manifest.d00b8c99471c6bb08311552ec2f80ed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13df9652f1cb57ad996ce1ad3b1b5e48",
+    "url": "/Build-a-Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "7852a87e2297530ec5b9",
+    "url": "/Build-a-Pomodoro-Clock/static/css/2.0b8fbf56.chunk.css"
+  },
+  {
+    "revision": "4cb9ce623395e81df281",
+    "url": "/Build-a-Pomodoro-Clock/static/css/main.e0c1bcf1.chunk.css"
+  },
+  {
+    "revision": "7852a87e2297530ec5b9",
+    "url": "/Build-a-Pomodoro-Clock/static/js/2.952563e5.chunk.js"
+  },
+  {
+    "revision": "4cb9ce623395e81df281",
+    "url": "/Build-a-Pomodoro-Clock/static/js/main.05d8a853.chunk.js"
+  },
+  {
+    "revision": "210cccdd1efac7b2165f",
+    "url": "/Build-a-Pomodoro-Clock/static/js/runtime~main.b68536ad.js"
+  },
+  {
+    "revision": "342c532d381f22ed18d983ae1b3b8c96",
+    "url": "/Build-a-Pomodoro-Clock/static/media/beep.342c532d.mp3"
+  }
+]);
